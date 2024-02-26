7ae70dfa27542235a3febb052c01bea73f509e8a
RaiN214 <161090652+RaiN214@users.noreply.github.com>
2024-02-26 18:12:15 -0400

    Add files via upload

--- DesktopCleaner.py
+++ DesktopCleaner.py
@@ -21,17 +21,18 @@ def move_files_to_folders(source_dir, destination_dir, excluded_files):
     for filename in os.listdir(source_dir):
         if filename != "Desktop Cleaner" and not filename.startswith("."):  # Exclude the script and hidden files
             file_path = os.path.join(source_dir, filename)
-            if os.path.isfile(file_path) and file_path not in excluded_files:
+            if os.path.isfile(file_path):
                 file_type = filename.split('.')[-1]  # Get file extension
-                files_moved_histogram[file_type] = files_moved_histogram.get(file_type, 0) + 1
+                if filename not in excluded_files:
+                    files_moved_histogram[file_type] = files_moved_histogram.get(file_type, 0) + 1
 
-                target_dir = os.path.join(destination_dir, file_type)
-                create_folder_if_not_exists(target_dir)
-                shutil.move(file_path, os.path.join(target_dir, filename))
+                    target_dir = os.path.join(destination_dir, file_type)
+                    create_folder_if_not_exists(target_dir)
+                    shutil.move(file_path, os.path.join(target_dir, filename))
 
-                files_moved_count += 1
-            else:
-                file_types_displaced.add(file_type)
+                    files_moved_count += 1
+                else:
+                    file_types_displaced.add(file_type)
 
     end_time = time.time()
     duration = end_time - start_time
@@ -39,13 +40,13 @@ def move_files_to_folders(source_dir, destination_dir, excluded_files):
     return files_moved_count, file_types_displaced, duration, files_moved_histogram
 
 
-def get_desktop_and_excluded_paths():
+def get_desktop_and_excluded_files():
     desktop_path = input("Enter the path of your desktop: ")
     excluded_files = []
     num_excluded_files = int(input("Enter the number of files you do not wish to move: "))
     for i in range(num_excluded_files):
-        file_path = input(f"Enter the path of file {i + 1}: ")
-        excluded_files.append(file_path)
+        file_name = input(f"Enter the name of file {i + 1} + its extension (ex:Formula Table.docx)")
+        excluded_files.append(file_name)
 
     return desktop_path, excluded_files
 
@@ -74,7 +75,7 @@ def load_config():
 def main():
     desktop_path, excluded_files = load_config()
     if desktop_path is None:
-        desktop_path, excluded_files = get_desktop_and_excluded_paths()
+        desktop_path, excluded_files = get_desktop_and_excluded_files()
         save_config(desktop_path, excluded_files)
 
     desktop_cleaner_path = os.path.join(desktop_path, "Desktop Cleaner")
@@ -84,18 +85,26 @@ def main():
 
     while True:
         print("\nMenu:")
+        print("1. About")
         print("2. Move files")
         print("3. Change desktop path and excluded files")
         print("4. Exit")
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            print("This algorithm will clean up your computer desktop!\n When prompted, enter your desktop path and that of the files you wish to spare.\n "
-                  "Once the program is done running, some information about the file will be ")
 
-        elif choice == '2'
+            print('''This algorithm will clean up your computer desktop!\n
+            When prompted, enter your desktop path and that of the files you wish to spare.\n
+            Once the program is done running, some information such as the "Time Taken" to move 
+            the item(s) and the type of file(s) moved will be displayed through a "Bar Chart" and a "Pie Chart" ''')
+
+        elif choice == '2':
             files_moved_count, file_types_displaced, duration, files_moved_histogram = move_files_to_folders(
                 desktop_path, desktop_cleaner_path, excluded_files)
+            if files_moved_count == 0:
+                print("No files to be moved!")
+                break
+
             print(f"\nSummary:")
             print(f"Number of files moved: {files_moved_count}")
             print(f"File types displaced: {', '.join(file_types_displaced)}")
@@ -109,7 +118,7 @@ def main():
 
             # Plotting histogram
             plt.figure(figsize=(10, 6))
-            plt.bar(files_moved_histogram.keys(), files_moved_histogram.values(), color='skyblue')
+            plt.bar(files_moved_histogram.keys(), files_moved_histogram.values(), color='blue')
             plt.xlabel('File Type')
             plt.ylabel('Number of Files')
             plt.title('Histogram of File Types Moved')
@@ -117,7 +126,7 @@ def main():
             plt.show()
 
         elif choice == '3':
-            desktop_path, excluded_files = get_desktop_and_excluded_paths()
+            desktop_path, excluded_files = get_desktop_and_excluded_files()
             save_config(desktop_path, excluded_files)
 
         elif choice == '4':
@@ -126,8 +135,6 @@ def main():
 
         else:
             print("Please enter a valid option!")
-            main()
-
 
 
 if __name__ == "__main__":
